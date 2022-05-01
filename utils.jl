af6ffea824e7a929c96ee2fd44789e982e122549
Jose Daniel Lara <jdlara@berkeley.edu>
2022-05-01 14:39:50 -0700

    add wp diagram utils

--- utils.jl
+++ utils.jl
@@ -39,3 +39,20 @@ function find_min_key(d::Dict, metric)
 
     return minval, minkey
 end
+
+function wp_to_dict(wp)
+    errors = Vector{Any}(undef,0)
+    times = Vector{Any}(undef,0)
+    for i in 1:length(wp)
+        push!(errors,wp[i].errors)
+        push!(times,wp[i].times)
+    end
+    names = reshape(wp.names,1,length(wp))
+    wp_data = Dict{String, Any}()
+    for (ix, name) in enumerate(names)
+        wp_data[name] = Dict{String, Any}()
+        wp_data[name]["error"] = errors[ix]
+        wp_data[name]["times"] = times[ix]
+    end
+    return wp_data
+end
