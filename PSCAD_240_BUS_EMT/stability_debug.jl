f280ef1b799ed604a0d795ce484cbf2ad44ff865
Jose Daniel Lara <jdlara@berkeley.edu>
2022-12-13 11:59:21 -0700

    add the time domain sim

--- PSCAD_240_BUS_EMT/stability_debug.jl
+++ PSCAD_240_BUS_EMT/stability_debug.jl
@@ -45,6 +45,23 @@ for i in findall(x -> real(x) > -Inf, ss.eigenvalues)
     println("state $i with λ=$(ss.eigenvalues[i]) has $(eig_state_map[i])")
 end
 
+sim_ref = Simulation(
+        MassMatrixModel,
+        sys,
+        "PSCAD_240_BUS_EMT",
+        (0.0, 20.0),
+        BranchTrip(1.0, Line, "BUS 1-BUS 3-i_1");
+        file_level = Logging.Error,
+        console_level = Logging.Error
+        )
+
+execute!(sim_ref, Rodas5P(), abstol = 1e-10)
+results_ref = read_results(sim_ref)
+
+t, v = get_voltage_magnitude_series(results_ref, 103)
+plot(t, v)
+
+
 include("debug_utils.jl")
 include("modifiy_system.jl")
 sys = exchange_device_ib("generator-2203-DP-gfl")
