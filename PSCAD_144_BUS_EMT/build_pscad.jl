e35c0a5d38970f91e0a3b3498ec6bafacb33d24e
Matt Bossart <bossart.matt@gmail.com>
2023-03-16 17:05:51 -0400

    script cleaned up and working with snapshots

--- PSCAD_144_BUS_EMT/build_pscad.jl
+++ PSCAD_144_BUS_EMT/build_pscad.jl
@@ -6,14 +6,33 @@ import LinearAlgebra
 using PowerSystems
 using PowerSimulationsDynamics
 using PyCall
+using Serialization
 using Conda
 using UnPack
 using DataFrames
 using CSV
+using Plots
+
 
 include(joinpath(@__DIR__, "..", "PSID2PSCAD", "_pscad_psid_conversion", "build_system.jl"))
-include(joinpath(@__DIR__, "..", "PSID2PSCAD", "_pscad_psid_conversion", "parameterize_system.jl"))
-include(joinpath(@__DIR__, "..", "PSID2PSCAD", "_pscad_psid_conversion", "simulation_extras.jl"))
+include(
+    joinpath(
+        @__DIR__,
+        "..",
+        "PSID2PSCAD",
+        "_pscad_psid_conversion",
+        "parameterize_system.jl",
+    ),
+)
+include(
+    joinpath(
+        @__DIR__,
+        "..",
+        "PSID2PSCAD",
+        "_pscad_psid_conversion",
+        "simulation_extras.jl",
+    ),
+)
 include(joinpath(@__DIR__, "..", "PSID2PSCAD", "_pscad_psid_conversion", "collect_data.jl"))
 include(joinpath(@__DIR__, "constants.jl"))
 
@@ -43,35 +62,139 @@ PP = pyimport("PSCAD_Python")
 pscad = PP.basic_pscad_startup()
 sleep(3)    #need to let the default files load in pscad before loading the workspace you want 
 
-sys = System(joinpath(@__DIR__, "psid_files", "144Bus.json"))  
+sys = System(joinpath(@__DIR__, "psid_files", "144Bus.json"))
 pscad_workspace_name = "workspace_144bus.pswx"
 pscad_case_name = "case_144bus"
-##
-############# BUILD AND SAVE A NEW SYSTEM #######################################
+
+#=  ############# BUILD AND SAVE A NEW SYSTEM #######################################
 include(joinpath(@__DIR__, "psid_files", "bus_details.jl")) #define bus_coords_144 
-pscad.load(PyObject(joinpath(@__DIR__, "pscad_files", pscad_workspace_name)))  #load workspace 
+pscad.load(PyObject(joinpath(@__DIR__, "pscad_files", pscad_workspace_name)))
 project =
     pscad.create_project(1, pscad_case_name, PyObject(joinpath(@__DIR__, "pscad_files"))) #create new project (1 for case, 2 for library)
-build_system(sys, project, bus_coords_144; add_gen_breakers = true, add_load_breakers = true, add_line_breakers = true, add_multimeters = true) #build the system (place the components)
+build_system(
+    sys,
+    project,
+    bus_coords_144;
+    add_gen_breakers = true,
+    add_load_breakers = true,
+    add_line_breakers = true,
+    add_multimeters = true,
+)
 project.save()
-##
-############# PARAMETERIZE EXISTING SYSTEM #######################################
-#pscad.load(PyObject(joinpath(@__DIR__, "pscad_files", pscad_workspace_name)))  #load workspace 
-#project = pscad.project(pscad_case_name) #load existing project 
-parameterize_system(sys, project)   #parameterize each component 
-project.save()
-##
-############# SETUP PERIPHERAL COMPONENTS ########################################
-#Setup Output Channels
-quantities_to_record = [(:P, pscad_compat_name("generator-13-1")), (:V, "Bus_53")]
+
+############# PARAMETERIZE EXISTING SYSTEM ####################################
+parameterize_system(sys, project) 
+project.save()  =#
+
+pscad.load(PyObject(joinpath(@__DIR__, "pscad_files", pscad_workspace_name)))  #load workspace 
+project = pscad.project(pscad_case_name) #load existing project 
+
+
+############# SETUP OUTPUT CHANNELS ###########################################
+quantities_to_record = [(:V, "Bus_53")]  #Note: Add more signals to save here (:P, pscad_compat_name("generator-13-1")),
 setup_output_channnels(project, quantities_to_record, (15, 2))
 
-#Setup breaker logic for perturbation (Don't save after setting breaker logic) 
-perturbation = BranchTrip(1.0, Line,  "Bus_56-Bus_54-i_1")
-setup_breaker_operations(project, perturbation)
+############# SETUP INITIALIZATION CHANNELS ###################################
+PP.update_parameter_by_name(project.find("master:const", "t_INV"), "Value", 0.5)
+PP.update_parameter_by_name(project.find("master:const", "t_GEN"), "Value", 0.5)
+PP.update_parameter_by_name(project.find("master:const", "t_RAMP"), "Value", 0.1)
 
-PP.update_parameter_by_name(project.find("master:const", "t_INV"), "Value", 2.0)
-PP.update_parameter_by_name(project.find("master:const", "t_GEN"), "Value", 2.0)
-PP.update_parameter_by_name(project.find("master:const", "t_RAMP"), "Value", 2.0)
+############ RUN TO STEADY STATE, RECORD DATA, CHECK SS CONDITIONS ############
+snapshot_name = "snap_4"
+snapshot_path_for_startup  = joinpath(@__DIR__, "pscad_files", "case_144bus.gf46", string(snapshot_name, ".snp"))#  "snapshot_2.snp")
 
-set_project_parameters!(project; time_duration =10.0, sample_step = 2e-4 * 1e6, time_step = 10e-5 * 1e6)
+from_snap = true    #false -> save a snapshot during run; true -> start from snapshot 
+
+if from_snap 
+    set_project_parameters!(
+        project;
+        startup_filename = snapshot_path_for_startup,
+        SnapType = 0,
+        StartType = 1,
+        time_duration = 0.01,
+        sample_step = 500e-6 * 1e6,
+        time_step = 25e-6 * 1e6,
+    ) 
+else 
+    set_project_parameters!(
+        project;
+        snapshot_filename = snapshot_name,
+        SnapType = 1,
+        SnapTime = 0.005,
+        time_duration = 0.015,
+        sample_step = 500e-6 * 1e6,
+        time_step = 25e-6 * 1e6,
+    )
+end 
+
+pscad_output_folder_path = joinpath(@__DIR__, "pscad_files", string(pscad_case_name, ".gf46"))
+foreach(rm, filter(!endswith(".snp"), readdir(pscad_output_folder_path,join=true))) #Don't delete snapshot file.
+
+sim_time = @timed project.run()
+
+sim = Simulation!(MassMatrixModel, sys, pwd(), (0.0, 0.0))
+x0_dict = read_initial_conditions(sim)
+snapshot_dict = Dict(read_initial_conditions(sim) => snapshot_name) # set snapshot name
+Serialization.serialize("snapshot_dict", snapshot_dict)
+save_directory = joinpath(@__DIR__, "results", "initialization")
+mkpath(save_directory)
+
+df = collect_pscad_outputs(pscad_output_folder_path)[1]  
+open(joinpath(save_directory, "pscad_output.csv"), "w") do io
+    CSV.write(io, df)
+end
+for signal_name in filter!(x-> x !== "time",names(df))
+    display(plot(df[1:end, "time"], df[1:end, signal_name], label = signal_name)) #ylim=(0.9,1.1), xlim=(3.0,3.2))
+end
+
+#TODO - CHECK INITIALIZATION WORKED AND SYSTEM IS IN STEADY STATE.
+##
+########### RUN SIMS AND SAVE DATA ############################################
+project = pscad.project(pscad_case_name)
+snapshot_dict = Serialization.deserialize("snapshot_dict")
+
+set_project_parameters!(
+    project;
+    time_duration = 10.0,
+    sample_step = 2e-4 * 1e6,
+    time_step = 10e-6 * 1e6,
+)
+
+perturbations = [BranchTrip(1.0, Line, "Bus_56-Bus_54-i_1")]      #Note: Add more perturbations here 
+for p in perturbations
+    folder_name = p.branch_name
+    mkpath(folder_name)
+    setup_breaker_operations(project, p)
+    xo_dict = read_initial_conditions(Simulation!(MassMatrixModel, sys, pwd(), (0.0, 0.0)))
+    if haskey(snapshot_dict, xo_dict)
+        snapshot_file_name = snapshot_dict[xo_dict]
+        set_project_parameters!(
+            project;
+            StartType = 1,
+            startup_filename = snapshot_file_name,
+        )
+        PP.update_parameter_by_name(project.find("master:const", "t_INV"), "Value", 0.0)
+        PP.update_parameter_by_name(project.find("master:const", "t_GEN"), "Value", 0.0)
+        PP.update_parameter_by_name(project.find("master:const", "t_RAMP"), "Value", 0.0)
+    else
+        @error "Snapshot file for given operating condition not found."
+    end
+    set_project_parameters!(
+        project;
+        time_duration = 10.0,
+        sample_step = 2e-4 * 1e6,
+        time_step = 10e-5 * 1e6,
+    )
+    project.run()
+    #TODO - make folder structure for systematic saving (name of folder = fault ran)
+    df = collect_pscad_outputs(pscad_output_folder_path)[1]
+    open(
+        joinpath(@__DIR__, "..", "pscad_files", string(output_csv_name, ".csv")),
+        "w",
+    ) do io
+        CSV.write(io, df)
+    end
+end
+
+pscad.quit()
+logging.shutdown()
