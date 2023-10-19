0525157a1c96ae35ad11ff4256666b19d62a87be
Jose Daniel Lara <jdlara@berkeley.edu>
2023-10-19 12:07:58 -0600

    update code

--- PSCAD_144_EMT/modify_144_sys.jl
+++ PSCAD_144_EMT/modify_144_sys.jl
@@ -1,7 +1,32 @@
 using PowerSystems
-
+using Random
+using PowerSimulationsDynamics
+using Sundials
+using Logging
+using PowerFlows
 
 sys = System("/Users/jlara/cache/PSIDValidation/PSCAD_144_EMT/144Bus.json")
+
+for b in get_components(Bus, sys)
+    @show get_name(b)
+    dev = get_components(x -> get_bus(x) == b && get_dynamic_injector(x) !== nothing, StaticInjection, sys)
+    if isempty(dev)
+        continue
+    end
+    base_power = sum(get_base_power(x) for x in dev)
+    dev_ = collect(dev)
+    @show length(dev_)
+    choosen_ix = max(1, sum(bitrand(length(dev_))))
+    @show choosen_ix
+    for (ix, d) in enumerate(dev_)
+        if ix != choosen_ix
+            remove_component!(sys, get_dynamic_injector(d))
+            remove_component!(sys, d)
+        end
+    end
+    set_base_power!(dev_[choosen_ix], base_power)
+    set_base_power!(get_dynamic_injector(dev_[choosen_ix]), base_power)
+end
 
 for b in get_components(DynamicGenerator, sys)
     static = get_component(ThermalStandard, sys, get_name(b))
@@ -12,4 +37,31 @@ for b in get_components(DynamicGenerator, sys)
     add_component!(sys, shunt)
 end
 
-to_json(sys, "/Users/jlara/cache/PSIDValidation/PSCAD_144_EMT/144Bus_with_shunts.json")
+solve_powerflow!(sys)
+
+line_to_trip = "Bus_7-Bus_5-i_1"
+t_sample =  5.0e-4 #* 1e6
+t_dynamic_sim = 5.0
+time_step_pscad = 25e-6 * 1e6
+
+for b in get_components(Line, sys)
+    if get_name(b) != line_to_trip
+        dyn_branch = PowerSystems.DynamicBranch(b)
+        add_component!(sys, dyn_branch)
+    end
+end
+
+perturbation = BranchTrip(0.1, Line, line_to_trip)
+sim = Simulation(
+    ResidualModel,
+    sys,
+    pwd(),
+    (0.0, t_dynamic_sim),
+    perturbation;
+    file_level =  Logging.Debug,
+    frequency_reference = ReferenceBus(),
+)
+# IDA won't work with KLU in this properly due to the use of random in the same script
+execute!(sim, IDA())
+
+to_json(sys, "/Users/jlara/cache/PSIDValidation/PSCAD_144_EMT/144Bus_with_shunts.json"; force = true)
--- PSCAD_144_EMT/rmse_144bus_analysis.jl
+++ PSCAD_144_EMT/rmse_144bus_analysis.jl
@@ -12,11 +12,11 @@ using Plots
 const PSY = PowerSystems
 
 ################ OPTIONS #################
-#GENERAL PARAMETERS WHICH APPLY TO BOTH PSID AND PSCAD 
-line_to_trip = "Bus_7-Bus_5-i_1" 
-t_sample =  5.0e-4 #* 1e6 
+#GENERAL PARAMETERS WHICH APPLY TO BOTH PSID AND PSCAD
+line_to_trip = "Bus_7-Bus_5-i_1"
+t_sample =  5.0e-4 #* 1e6
 t_dynamic_sim = 5.0
-time_step_pscad = 25e-6 * 1e6  
+time_step_pscad = 25e-6 * 1e6
 
 sys = System(joinpath(@__DIR__, string("144Bus", ".json")), runchecks = false)
 
@@ -25,6 +25,11 @@ for b in get_components(Line, sys)
         dyn_branch = PowerSystems.DynamicBranch(b)
         add_component!(sys, dyn_branch)
     end
+end
+
+for c in get_components(DynamicGenerator, sys)
+    machine = get_machine(c)
+    set_R!(machine, get_Xd_p(machine)*0.01)
 end
 
 perturbation = BranchTrip(0.1, Line, line_to_trip)
@@ -38,35 +43,39 @@ sim = Simulation(
     frequency_reference = ReferenceBus(),
 )
 
+eig = small_signal_analysis(sim)
+sei = summary_eigenvalues(eig)
+sei[sei[!, "Freq [Hz]"] .<= 65, :][10:35, :]
+
 #Run simulation with dtmax matching PSCAD timestep and saveat matching save interval from PSCAD
 sim_time_fixed = @timed execute!(sim, IDA(linear_solver = :KLU), maxiters=Int(1e6), dtmax = 25e-6, saveat=500e-6)
 @show sim_time_fixed
 result_psid_fixed = read_results(sim)
 ##
-#Run simulation with adaptive timestepping and small tolerance and save interval mathcing PSCAD. 
+#Run simulation with adaptive timestepping and small tolerance and save interval mathcing PSCAD.
 sim_time_adaptive = @timed execute!(sim, IDA(linear_solver = :KLU), saveat=500e-6, abstol=1e-9, reltol=1e-9)
 @show sim_time_adaptive
 result_psid_adaptive = read_results(sim)
 
 pscad_results_file = joinpath(@__DIR__, "pscad_results_dynamics.csv")
-pscad_results = CSV.read(pscad_results_file, DataFrame)  #header =2? 
+pscad_results = CSV.read(pscad_results_file, DataFrame)  #header =2?
 
 ### Store Errors in Dictionaries ###
 dict_voltage_fixed = Dict()
 dict_speed_fixed = Dict()
-dict_activepower_fixed = Dict() 
+dict_activepower_fixed = Dict()
 dict_reactivepower_fixed = Dict()
 dict_voltage_adaptive = Dict()
 dict_speed_adaptive = Dict()
-dict_activepower_adaptive = Dict() 
+dict_activepower_adaptive = Dict()
 dict_reactivepower_adaptive = Dict()
 
 function pscad_compat_name(psid_name)
     return replace(psid_name, "-" => "_")
-end 
+end
 
 
-#Section for storing RMSE for all traces 
+#Section for storing RMSE for all traces
 for bus in get_components(Bus, sys)
     bus_number = get_number(bus)
     bus_name = get_name(bus)
@@ -74,7 +83,7 @@ for bus in get_components(Bus, sys)
     #Compare for dtfixed
     t_psid, voltage_psid_fixed = get_voltage_magnitude_series(result_psid_fixed, bus_number)
     res_V_fixed = voltage_psid_fixed[2:end] .- voltage_pscad   #Don't have t=0 for PSCAD
-    dict_voltage_fixed[bus_name] =  LinearAlgebra.norm(res_V_fixed) / length(res_V_fixed) 
+    dict_voltage_fixed[bus_name] =  LinearAlgebra.norm(res_V_fixed) / length(res_V_fixed)
 
     #Compare for adaptive
     t_psid, voltage_psid_adaptive = get_voltage_magnitude_series(result_psid_adaptive, bus_number)
@@ -98,8 +107,8 @@ for d in get_components(DynamicInjection, sys)
     res_Q_fixed = Q_psid_fixed[2:end]  .- Q_pscad
     res_f_fixed = f_psid_fixed[2:end]  .- f_pscad
     dict_activepower_fixed[psid_name] = LinearAlgebra.norm(res_P_fixed) / length(res_P_fixed)
-    dict_reactivepower_fixed[psid_name] = LinearAlgebra.norm(res_Q_fixed) / length(res_Q_fixed) 
-    dict_speed_fixed[psid_name] =  LinearAlgebra.norm(res_f_fixed) / length(res_f_fixed) 
+    dict_reactivepower_fixed[psid_name] = LinearAlgebra.norm(res_Q_fixed) / length(res_Q_fixed)
+    dict_speed_fixed[psid_name] =  LinearAlgebra.norm(res_f_fixed) / length(res_f_fixed)
 
     #Compare for adaptive
     _, P_psid_adaptive = get_activepower_series(result_psid_adaptive, psid_name)
@@ -109,7 +118,7 @@ for d in get_components(DynamicInjection, sys)
     res_Q_adaptive = Q_psid_adaptive[2:end]  .- Q_pscad
     res_f_adaptive = f_psid_adaptive[2:end]  .- f_pscad
     dict_activepower_adaptive[psid_name] = LinearAlgebra.norm(res_P_adaptive) / length(res_P_adaptive)
-    dict_reactivepower_adaptive[psid_name] = LinearAlgebra.norm(res_Q_adaptive) / length(res_Q_adaptive) 
+    dict_reactivepower_adaptive[psid_name] = LinearAlgebra.norm(res_Q_adaptive) / length(res_Q_adaptive)
     dict_speed_adaptive[psid_name] =  LinearAlgebra.norm(res_f_adaptive) / length(res_f_adaptive)
 end
 
@@ -128,4 +137,3 @@ display(p)
 p = plot(dict_reactivepower_adaptive, label ="adaptive", title = "Q")
 plot!(p, dict_reactivepower_fixed, label = "fixed")
 display(p)
-
